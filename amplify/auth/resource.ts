334f104452670ab8af6f68dae9e71cc2497b3855
alexmolosh-pixel <alexmolosh@nga911.com>
2025-11-02 14:15:48 +0100

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,8 +6,13 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    phone: {
-      required: true,
-    }
+    phone: true
   },
+
+  userAttributes: {
+     phoneNumber: {
+         required: true,
+     }
+  }
+  
 });
